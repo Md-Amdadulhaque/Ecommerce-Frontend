81833f2578356e871e1ed1af4ce85f709ca38c96
Amdadul <amdadulhaque54321@gmail.com>
2025-12-07 13:36:22 +0600

    mcp

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 import { CategoryService } from '../Services/CategoryService/category.service';
 import { CommonModule } from '@angular/common';
 
+export interface Category{
+   Name: string;
+   Description: string;
+    ImageData: string | null;
+}
+
 @Component({
   selector: 'app-category',
   standalone: true,
@@ -12,13 +18,36 @@ import { CommonModule } from '@angular/common';
 export class CategoryComponent {
   title = "Category";
   data: any[] = [];
+  categories: Category[] = [];
 
-  constructor(private categoryService: CategoryService) {
-    this.categoryService.ShowCategory(this.title).subscribe((response: any) => {
-      this.data = response;
+  ngOnInit(): void {
+     if(this.data.length==0){
+      this.show();
+     }
+     else {
+      this.categories = this.data;
+     }
+  }
+  constructor(private categoryService: CategoryService) {}
+
+  show() {
+      this.categoryService.ShowCategory(this.title).subscribe((response: any) => {
+      this.categories = this.data;
     });
   }
-  show() {
 
+ getImageSrc(imageData: string | null): string {
+    if (imageData) {
+      return `data:image/jpeg;base64,${imageData}`;
+    }
+    return 'https://via.placeholder.com/80';
   }
+
+
+  viewCategory(category: Category): void {
+    console.log('Viewing category:', category);
+    // Navigate to products by category
+      ///this.router.navigate(['/Products', category.Name]);
+  }
+
 }
